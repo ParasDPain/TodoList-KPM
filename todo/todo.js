9ecd0c20678fac9e600b423c730d8eaae667cc35
parasdpain <parasdeep29@gmail.com>
2016-02-26 09:08:23 +1300

    Finished DisplayTodo

--- todo/todo.js
+++ todo/todo.js
@@ -5,7 +5,7 @@ var Users;
 // TODO backup json in exports.unload()
 exports.load = function () {
     try {
-        file = JSON.parse(require.safe('./../common/todoList.json'));
+        file = require.safe('./../common/todoList.json');
         
         // If file is empty for some weird reason
         if (!file.hasOwnProperty("users")) {
@@ -54,15 +54,15 @@ exports.run = function (api, event) {
 
         if (command.startsWith(commandPrefix + "todo " + commandPrefix + "add")) {
             query = command.replace(commandPrefix + "todo " + commandPrefix + "add", "");
-            AddTodoItem(userName, userId, query.trim());
+            api.SendMessage(AddTodoItem(userName, userId, query.trim()), event.thread_id);
 
         } else if (command.startsWith(commandPrefix + "todo " + commandPrefix + "remove")) {
             query = command.replace(commandPrefix + "todo " + commandPrefix + "add", "");
-            RemoveTodoItem(userName, userId, query.trim());
-            
-        } else if(command.startsWith(commandPrefix + "todo")){
+            api.SendMessage(RemoveTodoItem(userName, userId, query.trim()), event.thread_id);
+
+        } else if (command.startsWith(commandPrefix + "todo")) {
             query = command.replace(commandPrefix + "todo", "");
-            DisplayTodo(userName, userId);
+            api.SendMessage(DisplayTodo(userName, userId), event.thread_id);
         }
     } else {
         // JSON file not found, display appropriate message
@@ -80,8 +80,27 @@ function RemoveTodoItem(userName, userId, itemId) {
     // else display sarcastic message
 }
 
-function DisplayTodo(userName, userId){
-    // Search user with name and id, if exists
-    // Display all todo items of the requesting user ONLY
-    // else Display that no items in the list yet (be polite)
+// Display all todo items of the requesting user ONLY
+function DisplayTodo(userName, userId) {
+    var u = SearchUserById(userId);
+    if (u !== null) {
+        var message = "Todo List for " + u + "\n";
+        for (var i = 0; i < u.list.length; i++) {
+            message += u.list[i].item + "\n";
+        }
+        return message;
+    } else {
+        // else Display that no items in the list yet (be polite)
+        return "You don't have items in your list yet";
+    }
+}
+
+// Search user by Id in the local json records
+function SearchUserById(userId) {
+    for (var i = 0; i < Users.length; i++) {
+        if (Users[i].id === userId) {
+            return Users[i];
+        }
+    }
+    return null;
 }
